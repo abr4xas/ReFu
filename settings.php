4c45e6fe55bfebe900a696abec58099be31c1c94
abr4xas <bullgram@gmail.com>
2012-09-09 23:44:28 -0430

    Codigo ejemplo. Se agrega codigo de ejemplo al archivo.

--- settings.php
+++ settings.php
@@ -7,3 +7,9 @@ Author: abr4xas
 Author URI: http://abr4xas.org/refu/
 License: GPLv2 or later
 */
+?>
+<!-- main -->
+<div class="wrap">
+<h1> Aqu&iacut; va el titulo de algo</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam scelerisque nunc urna, ut rhoncus ligula. Vestibulum imperdiet odio in odio lobortis aliquet. Cras lorem ipsum, rutrum ut hendrerit a, placerat ac dolor. Nulla mollis, sapien sed rhoncus aliquam, magna arcu eleifend sapien, vel faucibus urna ligula id urna. Vestibulum vulputate lorem ut ipsum posuere at vulputate nibh pulvinar. Fusce non risus vel justo elementum blandit. Vivamus placerat dapibus nibh at fermentum. Fusce non nibh eros, sed vestibulum risus. In lectus ipsum, lacinia ut mattis et, consequat sit amet felis. Aliquam erat volutpat. In hac habitasse platea dictumst. Ut eleifend libero eu nibh porta vel consequat mauris eleifend. Vestibulum ornare pellentesque erat sit amet interdum. Praesent sit amet fringilla dui. Pellentesque eget massa mi, et vulputate enim. Fusce nisl turpis, molestie a vulputate non, aliquet in tortor.</p>
+</div> <!-- / main -->
