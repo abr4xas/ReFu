30ce34fcdc72e6df61971435b4135c4148669ae3
abr4xas <bullgram@gmail.com>
2012-09-09 20:39:24 -0430

    Settings Se agrega archivo de configuracion. Trabajo para la version 3.0 del plug-in

---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Functions
+Description: Alternative <code>functions.php</code>  file of wordpress themes.
+Version: 3.0
+Author: abr4xas
+Author URI: http://abr4xas.org/refu/
+License: GPLv2 or later
+*/
