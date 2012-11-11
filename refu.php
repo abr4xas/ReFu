c682ab8ebd0adba59459c3a7bd0a5d8b39e89617
abr4xas <webmaster@abr4xas.org>
2012-11-11 15:35:04 -0430

    Se actualiza core

--- refu.php
+++ refu.php
@@ -128,14 +128,12 @@ function twitter_oembed($a) {
 
 // Color according to different post state
 function posts_status_color() {
-?>
   <style>
   .status-draft { background: #FCE3F2 !important; }
   .status-pending { background: #87C5D6 !important; }
   .status-future { background: #C6EBF5 !important; }
   .status-private { background: #F2D46F; }
   </style>
-<?php
 }
 add_action('admin_footer','posts_status_color');
 
@@ -163,3 +161,4 @@ function disable_self_ping( &$links ) {
             unset($links[$l]);
 }
 add_action( 'pre_ping', 'disable_self_ping' );
+?>
