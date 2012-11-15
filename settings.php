087012e17786906341e3401f2ebda975af6f6696
abr4xas <webmaster@abr4xas.org>
2012-11-14 21:07:10 -0430

    Se actualiza core

--- settings.php
+++ settings.php
@@ -1,43 +1,23 @@
-<!-- main -->
-<div class="refu">
-<h1> Aqu&iacute; va el titulo de algo</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam scelerisque nunc urna, ut rhoncus ligula. Vestibulum imperdiet odio in odio lobortis aliquet. Cras lorem ipsum, rutrum ut hendrerit a, placerat ac dolor. Nulla mollis, sapien sed rhoncus aliquam, magna arcu eleifend sapien, vel faucibus urna ligula id urna. Vestibulum vulputate lorem ut ipsum posuere at vulputate nibh pulvinar. Fusce non risus vel justo elementum blandit. Vivamus placerat dapibus nibh at fermentum. Fusce non nibh eros, sed vestibulum risus. In lectus ipsum, lacinia ut mattis et, consequat sit amet felis. Aliquam erat volutpat. In hac habitasse platea dictumst. Ut eleifend libero eu nibh porta vel consequat mauris eleifend. Vestibulum ornare pellentesque erat sit amet interdum. Praesent sit amet fringilla dui. Pellentesque eget massa mi, et vulputate enim. Fusce nisl turpis, molestie a vulputate non, aliquet in tortor.</p>
-
-<table border="1" bordercolor="#000" style="background-color:#fff" width="80%" cellpadding="0" cellspacing="0">
-	<tr>
-		<td>Table Cell</td>
-		<td>Table Cell</td>
-	</tr>
-	<tr>
-		<td>Table Cell</td>
-		<td>Table Cell</td>
-	</tr>
-	<tr>
-		<td>Table Cell</td>
-		<td>Table Cell</td>
-	</tr>
-	<tr>
-		<td>Table Cell</td>
-		<td>Table Cell</td>
-	</tr>
-	<tr>
-		<td>Table Cell</td>
-		<td>Table Cell</td>
-	</tr>
-</table>
 <?php
-$rss = fetch_feed('http://feeds.feedburner.com/ElBlogDeAbr4xasVol2');
-$rss_items = $rss->get_items( 0, $rss->get_item_quantity(1) );
-if ( !$rss_items ) {
-	echo 'no items';
-} else {
-	foreach ( $rss_items as $item ) {
-		echo '<h2><a href="' . $item->get_permalink() . '">' . $item->get_title() . '</a></h2>';
-		echo '<p>' . $item->get_content() . '</p>';
-	}
-}
+
+    if (defined('ALLOW_INCLUDE') === false)
+        die('no direct access');
+
 ?>
 
+<!-- main -->
+<div id="refu">
+<div id="icon-edit" class="icon32"><br /></div>
+<h1> ReFu - Regulatory Functions panel</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam scelerisque nunc urna, ut rhoncus ligula. Vestibulum imperdiet odio in odio lobortis aliquet. Cras lorem ipsum, rutrum ut hendrerit a, placerat ac dolor. Nulla mollis, sapien sed rhoncus aliquam, magna arcu eleifend sapien, vel faucibus urna ligula id urna. Vestibulum vulputate lorem ut ipsum posuere at vulputate nibh pulvinar. Fusce non risus vel justo elementum blandit. Vivamus placerat dapibus nibh at fermentum. Fusce non nibh eros, sed vestibulum risus. In lectus ipsum, lacinia ut mattis et, consequat sit amet felis. Aliquam erat volutpat. In hac habitasse platea dictumst. Ut eleifend libero eu nibh porta vel consequat mauris eleifend. Vestibulum ornare pellentesque erat sit amet interdum. Praesent sit amet fringilla dui. Pellentesque eget massa mi, et vulputate enim. Fusce nisl turpis, molestie a vulputate non, aliquet in tortor.</p>
+<fieldset>
+<form action="">
+ <label for=”grav”>Newgravatar:</label> <input type="text" name="gravatar" id="grav"  size="40"  value="abr4xas"  tabindex="1" /><br>
+   <label for=”rss”>Custom feed link:</label> <input type="text" name="feed" id="rss" size="40"  value="feeds.feedburner.com/ElBlogDeAbr4xasVol2"  tabindex="2" /><br>
+ <label for=”fadmin”>Foter text admin panel:</label> <input type="text" name="footer" id="fadmin" size="40"  value="With wordpress and love <3" tabindex="3" /><br>
+ <p class="submit"> <input type="submit" value="Submit"></p>
+</form>
+</fieldset>
 </div> 
 
 
